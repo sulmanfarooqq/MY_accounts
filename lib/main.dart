e1cb7a8eb5a3caebc33375be17cbad0263ff261a
[sulmanfarooqq] <[sulmanfarooq27@gmail.com]>
2025-06-21 18:28:22 +0500

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'simple_link_page.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SimpleLinkPage(),
     ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,22 +7,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-// Update the import path below to match the actual location of your main.dart file.
-// For example, if main.dart is in lib/, use:
-// Update the import path below to match the actual location of your SimpleLinkPage class.
-// Make sure the import path matches the actual location of your SimpleLinkPage class.
-// For example, if SimpleLinkPage is defined in lib/pages/simple_link_page.dart, use:
-// TODO: Update the import path below to the correct location of SimpleLinkPage.
-// For example, if SimpleLinkPage is in lib/simple_link_page.dart, use:
-import 'package:futuristic_portfolio/simple_link_page.dart';
-// If the file does not exist, create it and define the SimpleLinkPage widget.
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/simple_link_page.dart';
 
 void main() {
-  testWidgets('FuturisticPortfolioPage has profile name', (WidgetTester tester) async {
+  testWidgets('SimpleLinkPage has profile name', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp(home: FuturisticPortfolioPage()));
+    await tester.pumpWidget(const MaterialApp(home: SimpleLinkPage()));
 
     // Verify that the profile name is displayed.
-    expect(find.text('Sulman Farooq'), findsOneWidget);
+    expect(find.text('Corinne'), findsOneWidget);
   });
 }
