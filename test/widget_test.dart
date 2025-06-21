84d49a55862216cf3cd315415059cfaa1953bb42
[sulmanfarooqq] <[sulmanfarooq27@gmail.com]>
2025-06-21 18:20:02 +0500

    new commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,22 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/main.dart';
-import '../lib/main.dart';
+// Update the import path below to match the actual location of your main.dart file.
+// For example, if main.dart is in lib/, use:
+// Update the import path below to match the actual location of your SimpleLinkPage class.
+// Make sure the import path matches the actual location of your SimpleLinkPage class.
+// For example, if SimpleLinkPage is defined in lib/pages/simple_link_page.dart, use:
+// TODO: Update the import path below to the correct location of SimpleLinkPage.
+// For example, if SimpleLinkPage is in lib/simple_link_page.dart, use:
+import 'package:futuristic_portfolio/simple_link_page.dart';
+// If the file does not exist, create it and define the SimpleLinkPage widget.
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('FuturisticPortfolioPage has profile name', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(home: FuturisticPortfolioPage()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the profile name is displayed.
+    expect(find.text('Sulman Farooq'), findsOneWidget);
   });
 }
